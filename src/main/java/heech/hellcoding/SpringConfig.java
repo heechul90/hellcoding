676fb91126e79a1772826fe4af5113f2afdfe82d
hclee <hclee@dataeum.kr>
2021-12-05 17:22:02 +0900

    순수 jdbc code

--- src/main/java/heech/hellcoding/SpringConfig.java
+++ src/main/java/heech/hellcoding/SpringConfig.java
@@ -1,5 +1,6 @@
 package heech.hellcoding;
 
+import heech.hellcoding.member.domain.Member;
 import heech.hellcoding.member.repository.*;
 import heech.hellcoding.member.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -19,23 +20,31 @@ public class SpringConfig {
         this.dataSource = dataSource;
     }*/
 
-    private final EntityManager em;
+    /*private final EntityManager em;
 
     public SpringConfig(EntityManager em) {
         this.em = em;
+    }*/
+
+    private final MemberRepository memberRepository;
+
+    @Autowired
+    public SpringConfig(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
     }
 
     @Bean
     public MemberService memberService() {
-        return new MemberService(memberRepository());
+        return new MemberService(memberRepository);
     }
 
-    @Bean
+/*    @Bean
     public MemberRepository memberRepository() {
         //return new MemoryMemberRepository();
         //return new JdbcMemberRepository();
         //return new JdbcTemplateMemberRepository(dataSource);
-        return new JpaMemberRepository(em);
-    }
+        //return new JpaMemberRepository(em);
+
+    }*/
 
 }
