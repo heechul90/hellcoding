d0931cd136ec0c4fa3866605e5174375ac826bd5
hclee <hclee@dataeum.kr>
2021-12-08 20:40:54 +0900

    user rest api - putMapping 추가중

--- src/main/java/heech/hellcoding/user/controller/UserController.java
+++ src/main/java/heech/hellcoding/user/controller/UserController.java
@@ -3,9 +3,12 @@ package heech.hellcoding.user.controller;
 import heech.hellcoding.user.domain.User;
 import heech.hellcoding.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.config.ConfigDataResourceNotFoundException;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/user")
@@ -22,6 +25,16 @@ public class UserController {
     @PostMapping("/insert")
     public User insertUser(@RequestBody User user) {
         return userService.insertUser(user);
+    }
+
+    @PutMapping("/update/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable(value = "id") Long id, @RequestBody User user) {
+        Optional<User> findUser = userService.getUser(id);
+        if (!findUser.isPresent()) {
+            throw new IllegalStateException(id + " 은 없는 id 입니다.");
+        }
+        //final User updateUser = userService.insertUser(user);
+        return null;
     }
 
     @DeleteMapping("/delete/{id}")
