d7f9b25018fe36643021b27af76d17edd131d71f
hclee <hclee@dataeum.kr>
2021-12-12 22:16:24 +0900

    application.properties 수정

--- src/main/java/heech/hellcoding/config/WebConfig.java
+++ src/main/java/heech/hellcoding/config/WebConfig.java
@@ -1,4 +1,4 @@
-package heech.hellcoding.user.config;
+package heech.hellcoding.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
