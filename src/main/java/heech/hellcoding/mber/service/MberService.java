a004d3b38406ae8dd40360f29bf9fd6f7d235658
hclee <hclee@dataeum.kr>
2022-02-10 14:04:22 +0900

    mber 인터페이스 및 구현체 생성

--- src/main/java/heech/hellcoding/mber/service/MberService.java
+++ src/main/java/heech/hellcoding/mber/service/MberService.java
@@ -1,4 +1,10 @@
 package heech.hellcoding.mber.service;
 
+import heech.hellcoding.mber.entity.Mber;
+
 public interface MberService {
+
+    void join(Mber mber);
+
+    Mber findMber(Long mberId);
 }
--- src/main/java/heech/hellcoding/mber/service/MberServiceImpl.java
+++ src/main/java/heech/hellcoding/mber/service/MberServiceImpl.java
@@ -1,4 +1,20 @@
 package heech.hellcoding.mber.service;
 
-public class MberServiceImpl {
+import heech.hellcoding.mber.entity.Mber;
+import heech.hellcoding.mber.repository.MberRepository;
+import heech.hellcoding.mber.repository.MemoryMberRepository;
+
+public class MberServiceImpl implements MberService {
+
+    private final MberRepository mberRepository = new MemoryMberRepository();
+
+    @Override
+    public void join(Mber mber) {
+        mberRepository.save(mber);
+    }
+
+    @Override
+    public Mber findMber(Long mberId) {
+        return mberRepository.findById(mberId);
+    }
 }
