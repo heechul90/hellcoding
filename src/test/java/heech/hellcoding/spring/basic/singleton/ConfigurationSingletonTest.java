716d512b295266e542564d402f504b94ae68a067
hclee <hclee@dataeum.kr>
2022-02-14 11:25:54 +0900

    싱글톤 테스트

---
+++ src/test/java/heech/hellcoding/spring/basic/singleton/ConfigurationSingletonTest.java
@@ -0,0 +1,46 @@
+package heech.hellcoding.spring.basic.singleton;
+
+import heech.hellcoding.spring.basic.AppConfig;
+import heech.hellcoding.spring.basic.member.repository.MemoryMemberRepository;
+import heech.hellcoding.spring.basic.member.service.MemberServiceImpl;
+import heech.hellcoding.spring.basic.order.service.OrderServiceImpl;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class ConfigurationSingletonTest {
+
+    @Test
+    void configurationTest() {
+
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemoryMemberRepository memberRepository = ac.getBean("memberRepository", MemoryMemberRepository.class);
+
+        // 모두 같은 인스턴스를 참고하고 있다.
+        System.out.println("memberService -> memberRepository = " + memberService.getMemberRepository());
+        System.out.println("orderService -> memberRepository = " + orderService.getMemberRepository());
+        System.out.println("memberRepository = " + memberRepository);
+
+        // 모두 같은 인스턴스를 참고하고 있다.
+        assertThat(memberService.getMemberRepository()).isSameAs(memberRepository);
+        assertThat(orderService.getMemberRepository()).isSameAs(memberRepository);
+    }
+
+    @Test
+    void configurationDeepTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        // AppConfig도 스프링 빈으로 등록된다.
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+
+        // 출력: bean = class heech.hellcoding.spring.basic.AppConfig$$EnhancerBySpringCGLIB$$ed20317a
+
+    }
+}
