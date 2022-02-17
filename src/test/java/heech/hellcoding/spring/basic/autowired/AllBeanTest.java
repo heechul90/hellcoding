765aed13f859c0b1b338e69e46ce4f5dc14022fc
hclee <heechul4296@gmail.com>
2022-02-17 09:13:55 +0900

    adding select all beans

--- src/test/java/heech/hellcoding/spring/basic/autowired/AllBeanTest.java
+++ src/test/java/heech/hellcoding/spring/basic/autowired/AllBeanTest.java
@@ -1,4 +1,19 @@
 package heech.hellcoding.spring.basic.autowired;
 
+import heech.hellcoding.spring.basic.AutoAppConfig;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
 public class AllBeanTest {
+
+    @Test
+    void findAllBean() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+    }
+
+
+    static class DiscountService {
+
+    }
+
 }
