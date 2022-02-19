2cdbc5faa284ef3169ff5c7ccf2ffa2b15e7c0b5
hclee <heechul4296@gmail.com>
2022-02-19 12:52:50 +0900

    빈을 list에 담기

--- src/test/java/heech/hellcoding/spring/basic/autowired/AllBeanTest.java
+++ src/test/java/heech/hellcoding/spring/basic/autowired/AllBeanTest.java
@@ -1,19 +1,57 @@
 package heech.hellcoding.spring.basic.autowired;
 
 import heech.hellcoding.spring.basic.AutoAppConfig;
+import heech.hellcoding.spring.basic.discount.policy.DiscountPolicy;
+import heech.hellcoding.spring.basic.member.entity.Grade;
+import heech.hellcoding.spring.basic.member.entity.Member;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import java.util.List;
+import java.util.Map;
+
+import static org.assertj.core.api.Assertions.*;
 
 public class AllBeanTest {
 
     @Test
     void findAllBean() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(AutoAppConfig.class, DiscountService.class);
+
+        DiscountService discountService = ac.getBean(DiscountService.class);
+
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        int fixDiscountPolcy = discountService.discount(member, 10000, "fixDiscountPolicy");
+
+        assertThat(discountService).isInstanceOf(DiscountService.class);
+        assertThat(fixDiscountPolcy).isEqualTo(1000);
+
+
     }
 
 
     static class DiscountService {
 
+        private final Map<String, DiscountPolicy> policyMap;
+        private final List<DiscountPolicy> policies;
+
+        public DiscountService(Map<String, DiscountPolicy> policyMap, List<DiscountPolicy> policies) {
+            this.policyMap = policyMap;
+            this.policies = policies;
+            System.out.println("policyMap = " + policyMap);
+            System.out.println("policies = " + policies);
+        }
+
+        public int discount(Member member, int price, String discountCode) {
+
+            DiscountPolicy discountPolicy = policyMap.get(discountCode);
+
+            System.out.println("discountCode = " + discountCode);
+            System.out.println("discountPolicy = " + discountPolicy);
+
+            return discountPolicy.discount(member, price);
+        }
     }
 
 }
